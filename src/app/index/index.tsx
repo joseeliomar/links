155666ccbfd5c8e9a9078a92c901d338c5c20099
José Eliomar <jose.eliomar2244@gmail.com>
2024-12-15 19:09:59 -0300

    Criação de novos componentes de tela e de mais uma nova tela

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,24 +1,63 @@
-import { View, Image, TouchableOpacity } from "react-native"
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
-import { colors} from "@/styles/colors"
+import { colors } from "@/styles/colors"
+import { Option } from "@/components/option"
 
-import { Category } from "@/components/category"
+import { Link } from "@/components/link"
+import { Categories } from "@/components/categories"
 
 export default function Index() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
-                <TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => router.navigate("./add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
 
-            <Category name="Projeto" icon="code" />
-            <Category name="Site" icon="language" />
-            <Category name="Vídeo" icon="movie" />
+            <Categories />
+
+            <FlatList 
+                data={["1", "2", "3", "4", "5"]}
+                keyExtractor={(item) => item}
+                renderItem={() => (
+                    <Link 
+                        name="Rocketseat" 
+                        url="https://www.rocketseat.com.br"
+                        onDetails={() => console.log("Clicou!")}
+                    />                    
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
+
+            <Modal transparent visible={false}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+                            
+                            <TouchableOpacity>
+                                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.modalLinkName}>Rocketseat</Text>
+                        <Text style={styles.modalUrl}>https://www.rocketseat.com.br</Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Abrir" icon="language" />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
--- src/components/option/styles.ts
+++ src/components/option/styles.ts
@@ -1,13 +1,21 @@
 import { StyleSheet } from "react-native";
 
+import { colors } from "@/styles/colors";
+
 export const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         alignItems: "center",
         gap: 5,
     },
-    name: {
+    primaryTitle: {
+        color: colors.green[300],
         fontSize: 16,
         fontWeight: "600",
     },
+    secondaryTitle: {
+        color: colors.gray[400],
+        fontSize: 16,
+        fontWeight: "600",
+    }
 })
