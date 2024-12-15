3b38a78d5ef53ef7a730e454f403c1d0244a5aef
José Eliomar <jose.eliomar2244@gmail.com>
2024-12-14 21:14:42 -0300

    Primeiro commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,24 @@
+import { View, Image, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { colors} from "@/styles/colors"
+
+import { Category } from "@/components/category"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
+
+            <Category name="Projeto" icon="code" />
+            <Category name="Site" icon="language" />
+            <Category name="Vídeo" icon="movie" />
+        </View>
+    )
+}
