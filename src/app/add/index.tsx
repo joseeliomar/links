f0df31d6862e30a12579349dfef907a33683bb81
José Eliomar <jose.eliomar2244@gmail.com>
2024-12-17 15:17:34 -0300

    Versão do app mobile construido no curso

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,13 +1,54 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router"
 
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
+import { linkStorage } from "@/storage/link-storage";
 
 import { Categories } from "@/components/categories";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
 
 export default function Add() {
+    const [category, setCategory] = useState("")
+    const [name, setName] = useState("")
+    const [url, setUrl] = useState("")
+
+    async function handleAdd() {
+        try {
+            if (!category) {
+                return Alert.alert("Categoria", "Selecione a categoria")
+            }
+    
+            if (!name.trim()) {
+                return Alert.alert("Nome", "Informe o nome")
+            }
+    
+            if (!url.trim()) {
+                return Alert.alert("URL", "Informe a URL")
+            }
+
+            await linkStorage.save({
+                id: new Date().getTime().toString(),
+                name,
+                url,
+                category,
+            })
+
+            Alert.alert("Sucesso", "Novo link adicionado", [
+                {
+                    text: "OK", 
+                    onPress: () => router.back(),
+                },
+            ])
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível salvar o link")
+            console.log(error)
+        }
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -19,7 +60,18 @@ export default function Add() {
             </View>
 
             <Text style={styles.label}>Selecione uma categoria</Text>
-            <Categories />
+            <Categories onChange={setCategory} selected={category}/>
+
+            <View style={styles.form}>
+                <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+                <Input 
+                    placeholder="URL" 
+                    onChangeText={setUrl} 
+                    autoCorrect={false} 
+                    autoCapitalize="none"
+                />
+                <Button title="Adicionar" onPress={handleAdd} />
+            </View>
         </View>
     )
 }
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,15 +1,72 @@
-import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
+import { useState, useCallback } from "react";
+import { View, Image, TouchableOpacity, FlatList, Modal, Text, Alert, Linking } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
-import { router } from "expo-router"
+import { router, useFocusEffect } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
-import { Option } from "@/components/option"
+import { categories } from "@/utils/categories";
+import { linkStorage, LinkStorage } from "@/storage/link-storage";
 
 import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
 
 export default function Index() {
+    const [showModal, setShowModal] = useState(false)
+    const [link, setLink] = useState<LinkStorage>({} as LinkStorage)
+    const [links, setLinks] = useState<LinkStorage[]>([])
+    const [category, setCategory] = useState(categories[0].name)
+
+    async function getLinks() {
+        try {
+            const response = await linkStorage.get()
+            const filtered = response.filter((link) => link.category === category)
+            setLinks(filtered)
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível listar os links")
+        }
+    }
+
+    function handleDetails(selected: LinkStorage) {
+        setShowModal(true)
+        setLink(selected)
+    }
+
+    async function linkRemove() {
+        try {
+            await linkStorage.remove(link.id)
+            getLinks()
+            setShowModal(false)
+        } catch (error) {
+            Alert.alert("Erro", "Não foi possível excluir")
+            console.log(error)
+        }
+    }
+
+    function handleRemove() {
+        Alert.alert("Excluir", "Deseja realmente excluir?", [
+            {style: "cancel", text: "Não"},
+            {text: "Sim", onPress: linkRemove},
+        ])
+    }
+
+    async function handleOpen() {
+        try {
+            await Linking.openURL(link.url)
+            setShowModal(false)
+        } catch (error) {
+            Alert.alert("Link", "Não foi possível abrir o link")
+            console.log(error)
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            getLinks()
+        }, [category])
+    )
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -20,16 +77,16 @@ export default function Index() {
                 </TouchableOpacity>
             </View>
 
-            <Categories />
+            <Categories onChange={setCategory} selected={category}/>
 
             <FlatList 
-                data={["1", "2", "3", "4", "5"]}
-                keyExtractor={(item) => item}
-                renderItem={() => (
+                data={links}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
                     <Link 
-                        name="Rocketseat" 
-                        url="https://www.rocketseat.com.br"
-                        onDetails={() => console.log("Clicou!")}
+                        name={item.name}
+                        url={item.url}
+                        onDetails={() => handleDetails(item)}
                     />                    
                 )}
                 style={styles.links}
@@ -37,23 +94,23 @@ export default function Index() {
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={false}>
+            <Modal transparent visible={showModal} animationType="slide">
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
-                            <Text style={styles.modalCategory}>Curso</Text>
+                            <Text style={styles.modalCategory}>{link.category}</Text>
                             
-                            <TouchableOpacity>
+                            <TouchableOpacity onPress={() => setShowModal(false)}>
                                 <MaterialIcons name="close" size={20} color={colors.gray[400]} />
                             </TouchableOpacity>
                         </View>
 
-                        <Text style={styles.modalLinkName}>Rocketseat</Text>
-                        <Text style={styles.modalUrl}>https://www.rocketseat.com.br</Text>
+                        <Text style={styles.modalLinkName}>{link.name}</Text>
+                        <Text style={styles.modalUrl}>{link.url}</Text>
 
                         <View style={styles.modalFooter}>
-                            <Option name="Excluir" icon="delete" variant="secondary" />
-                            <Option name="Abrir" icon="language" />
+                            <Option name="Excluir" icon="delete" variant="secondary" onPress={handleRemove}/>
+                            <Option name="Abrir" icon="language" onPress={handleOpen} />
                         </View>
                     </View>
                 </View>
